de91632b781286db4086b5fce160dd2b004044a4
Italo Costa <italocostajobs@gmail.com>
2024-07-30 13:50:07 -0300

    First task

---
+++ task-1/app.js
@@ -0,0 +1,5 @@
+const http = require('http');
+const routes = require('./routes');
+
+const server = http.createServer(routes.routesHandler);
+server.listen(3000);
---
+++ task-1/routes.js
@@ -0,0 +1,49 @@
+const routesHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Task 1</title></head>");
+    res.write("<body>");
+    res.write("<h1>Welcome to Task 1</h1>");
+    res.write(
+      "<form action='/create-user' method='POST'><input type='text' name='username'><button type='submit'>Submit</button></form>"
+    );
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/users") {
+    res.write("<html>");
+    res.write("<head><title>Task 1</title></head>");
+    res.write("<body><ul><li>User 1</li></ul></body>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/create-user" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const username = parsedBody.split("=")[1];
+      console.log(username);
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
+    });
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title><head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
+};
+
+exports.routesHandler = routesHandler;
